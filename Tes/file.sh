2c2b0b4f238b5fd8704359b63d1f77e96318e09e
Raffajah <Rafaalghifari205@gmail.com>
2025-11-27 13:00:42 +0700

    shel 2

--- Tes/file.sh
+++ Tes/file.sh
@@ -1,5 +1,4 @@
 teksaawal="halo ini saya rafa alghifari mengetik dengan kode shell, ini hanya ppecobaan saja dengan menggunakan shell variabel"
 teksdua="ini adalah teks kedua yang di buat dengan shell script"
-echo $teksaawal > Tes/file.sh
+echo $teksaawal
 echo $teksdua > Tes/file.sh
-
