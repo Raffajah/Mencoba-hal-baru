0c1aed7711fa1d5b0bf2be67efc7bc8415130f29
Raffajah <Rafaalghifari205@gmail.com>
2025-11-27 13:08:09 +0700

    varibabel sh tiga

--- Tes/file.sh
+++ Tes/file.sh
@@ -1,4 +1,8 @@
 teksaawal="halo ini saya rafa alghifari mengetik dengan kode shell, ini hanya ppecobaan saja dengan menggunakan shell variabel"
 teksdua="ini adalah teks kedua yang di buat dengan shell script"
-echo $teksaawal
-echo $teksdua > Tes/file.sh
+tekstiga="ini adalah teks ketiga yang di buat dengan shell script"
+
+echo $teksaawal >hasil.txt 
+echo $teksdua >>hasil.txt
+echo $tekstiga >>hasil.txt
+echo "selesai membuat file dengan shell script"
